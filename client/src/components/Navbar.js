be9b70d60ec82dfcc90257078bdd75bd1018d672
Robert Banica <roadstar93@gmail.com>
2020-02-02 22:44:35 +0200

    Style navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,56 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 export default function Navbar() {
-    return (
-        <div>
-            <h1>Navbar component</h1>
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Button color="inherit">
             <Link to="/">Home</Link>
-            <Link to="/signup">Signup</Link>
-            <Link to="/login">Login</Link>
+          </Button>
+          <Button color="inherit">
             <Link to="/products">Products</Link>
+          </Button>
+          <Button color="inherit">
             <Link to="/contact">Contact</Link>
-        </div>
-    )
+          </Button>
+          <Typography variant="h6" className={classes.title}></Typography>
+          <Button color="inherit"><Link to="/signup">Signup</Link></Button>
+          <Button color="inherit">
+            <Link to="/login">Login</Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
