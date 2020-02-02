aa2991dd86f0db9da4da440d90dfd86019eeb84c
Robert Banica <roadstar93@gmail.com>
2020-02-02 16:40:27 +0200

    Add database connection

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
+const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 
@@ -11,6 +12,7 @@ const API_PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(flash());
 app.use(
   require("express-session")({
     secret: "A special secret",
@@ -25,6 +27,19 @@ app.use(passport.session());
 // passport.use(new LocalStrategy(User.authenticate()));
 // passport.serializeUser(User.serializeUser());
 // passport.deserializeUser(User.deserializeUser());
+
+//Pass user info to all routes
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
+
+const dbConnection =
+  process.env.MONGODB_URI || "mongodb://localhost/shopalot-v1";
+mongoose.connect(dbConnection, { useNewUrlParser: true }).catch(error => handleError(error));
 
 if (process.env.NODE_ENV === "production") {
   //Set static folder
