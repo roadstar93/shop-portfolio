2eba8ad02cc1920178483b0adf8e5323a18a257f
Robert Banica <roadstar93@gmail.com>
2020-03-08 23:33:34 +0200

    Add form to AddProd page

--- client/src/components/AddProd.js
+++ client/src/components/AddProd.js
@@ -13,14 +13,53 @@ const useStyles = makeStyles(theme => ({
 
 export default function AddProd() {
   const classes = useStyles();
+  const [textfields, setTextFields] = React.useState({
+    Title: "",
+    Price: Number,
+    Category: "",
+    Description: "",
+    Image: ""
+  });
+
+  const handleChange = e => {
+    setTextFields({ ...textfields, [e.target.name]: e.target.value });
+  };
 
   return (
     <div>
       <h1>Add prod page</h1>
       <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Name" />
-        <TextField id="standard-basic" label="Price" />
-        <TextField id="standard-basic" label="Category" />
+        <TextField
+          onChange={handleChange}
+          name="Title"
+          id="standard-basic"
+          label="Title"
+        />
+        <TextField
+          onChange={handleChange}
+          name="Price"
+          id="standard-basic"
+          label="Price"
+        />
+        <TextField
+          onChange={handleChange}
+          name="Category"
+          id="standard-basic"
+          label="Category"
+        />
+        <TextField
+          onChange={handleChange}
+          id="standard-basic"
+          name="Image"
+          label="Image"
+        />
+        <TextField
+          id="soutlined-multiline-static"
+          multiline
+          rows="4"
+          name="Description"
+          label="Description"
+        />
       </form>
     </div>
   );
