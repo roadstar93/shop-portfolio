3144d0093253d4b9c81294ee6df987eb4f376c34
Robert Banica <roadstar93@gmail.com>
2020-03-09 21:19:54 +0200

    Add axios and handleSubmit functions

--- client/src/components/AddProd.js
+++ client/src/components/AddProd.js
@@ -1,5 +1,7 @@
 import React from "react";
+import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(theme => ({
@@ -15,7 +17,7 @@ export default function AddProd() {
   const classes = useStyles();
   const [textfields, setTextFields] = React.useState({
     Title: "",
-    Price: Number,
+    Price: "",
     Category: "",
     Description: "",
     Image: ""
@@ -25,10 +27,39 @@ export default function AddProd() {
     setTextFields({ ...textfields, [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = e => {
+    const output = {
+      Title: textfields.Title,
+      Price: textfields.Price,
+      Category: textfields.Category,
+      Description: textfields.Description,
+      Image: textfields.Image
+    };
+
+    try {
+      axios.post("/send", output);
+    } catch (error) {
+      alert(error.message);
+    }
+
+    setTextFields({
+      Title: "",
+      Price: "",
+      Category: "",
+      Description: "",
+      Image: ""
+    });
+  };
+
   return (
     <div>
       <h1>Add prod page</h1>
-      <form className={classes.root} noValidate autoComplete="off">
+      <form
+        onSubmit={handleSubmit}
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+      >
         <TextField
           onChange={handleChange}
           name="Title"
@@ -60,6 +91,9 @@ export default function AddProd() {
           name="Description"
           label="Description"
         />
+        <Button type="submit" color="primary">
+          Send
+        </Button>
       </form>
     </div>
   );
