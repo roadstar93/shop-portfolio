f908d4e2b62e739f797d9f97d0cae99be57610a3
Robert Banica <roadstar93@gmail.com>
2020-03-21 18:15:05 +0200

    Replace styles in Product component with bootstrap

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,36 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
+
 
 export default function Product({ product }) {
-  const classes = useStyles();
+
 
   const handleDelete = () => {
-  
-
     //Send updated to server
     try {
       axios.delete(`//localhost:3001/api/deleteProd/${product._id}`); // axios.post("//localhost:3001/api/addProd", output); used for dev enviroment testing
@@ -42,37 +21,21 @@ export default function Product({ product }) {
 
   return (
     <div>
-      <Card key={product._id} className={classes.root}>
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            Testing stuff
-          </Typography>
-          <Typography variant="h5" component="h2">
-            {product.title}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {product.category}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {product.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">
-            {" "}
+      <Card key={product._id} style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+          <Button variant="outline-primary">
             <Link to={`/products/editItem/${product._id}`}>Edit</Link>
           </Button>
-          <Button size="small">
+          <Button variant="outline-primary">
             <Link to={`/products/${product._id}`}>Go to product</Link>
           </Button>
-          <Button onClick={handleDelete} size="small">
-            Delete
+          <Button onClick={handleDelete} variant="primary">
+           Delete
           </Button>
-        </CardActions>
+        </Card.Body>
       </Card>
     </div>
   );
