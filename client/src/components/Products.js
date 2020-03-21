1041ebfb81eb7203bdd57bddaa51af9fde4e54cc
Robert Banica <roadstar93@gmail.com>
2020-03-21 18:24:31 +0200

    Add styles to Products component

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Product from "./Product";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function Products() {
-
   const [products, setProducts] = useState([]);
 
   async function getDataFromDB() {
@@ -21,10 +23,15 @@ function Products() {
   return (
     <div>
       <h1>Products</h1>
-
-      {products.map(product => (
-        <Product key={product._id} product={product} />
-      ))}
+      <Container fluid>
+        <Row>
+          {products.map(product => (
+            <Col xs={10} md={6} lg={4} xl={3} className="mb-3">
+              <Product key={product._id} product={product} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
       <Button onClick={getDataFromDB} size="small">
         Get data
       </Button>
