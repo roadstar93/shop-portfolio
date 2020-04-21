aa383de5d466ead1779f1d8289d5d5ef2d9ca4a0
Banica Robert <roadstar93@gmail.com>
2020-04-21 11:09:44 +0300

    Get data from local storage in shopping cart component

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ShoppingCart = () => {
-    return (
-        <div>
-            <h1>Shopping Cart</h1>
-        </div>
-    );
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = () => {
+      let allproducts = JSON.parse(localStorage.getItem("products"));
+      setProducts(allproducts);
+    };
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+      {products.map((product, id)=>(
+          <h1 key={id}>Title: {product.title}</h1>
+      ))}
+    </div>
+  );
+};
 
 export default ShoppingCart;
