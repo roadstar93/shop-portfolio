132beae5b3aae6c4488abf9f33e37ed13163c855
Banica Robert <roadstar93@gmail.com>
2020-08-22 17:22:30 +0300

    Style Shopping cart

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Router>
         <ProductProvider>
           <UserProvider>
--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -52,65 +52,70 @@ const ShoppingCart = () => {
     getProducts();
   }, []);
   return (
-    <Container fluid="lg" className="mb-3">
-      <h1>Shopping Cart</h1>
-      <Row>
-        {products.map((product, index) => (
-          <Col key={index} xs={12} className="products">
-            <div className="product-img">
-              <Button
-                onClick={() => {
-                  deleteItem(product.id)
-                }}
-                variant="primary"
-              >
-                Remove
-              </Button>
-              <img src={product.images[0]} alt={product.title} />
-              <Link to={`/products/${product.id}`}>
-                <h4>{product.title}</h4>
-              </Link>
-            </div>
-            <div className="product-details">
-              <Form.Group
-                onChange={handleChange}
-                className="product-amount"
-                controlId="formGroupEmail"
-              >
-                <Form.Label className="pr-1">Quantity:</Form.Label>
-                <Form.Control
-                  type="number"
-                  name={product.id}
-                  defaultValue={product.amount}
-                  as="select"
+    <div>
+      <h1 id="title">Sh<span>o</span>pping Cart</h1>
+      <Container fluid="lg" className="cart-container mb-3">
+        <Row>
+          {products.map((product, index) => (
+            <Col key={index} xs={12} className="products">
+              <div className="product-img">
+                <Button
+                  onClick={() => {
+                    deleteItem(product.id);
+                  }}
+                  variant="primary"
                 >
-                  <option>{product.amount}</option>
-                  <option onClick={() => handleSubmit(product.id)}>1</option>
-                  <option onClick={() => handleSubmit(product.id)}>2</option>
-                  <option onClick={() => handleSubmit(product.id)}>3</option>
-                  <option onClick={() => handleSubmit(product.id)}>4</option>
-                  <option onClick={() => handleSubmit(product.id)}>5</option>
+                  Remove
+                </Button>
+                <img src={product.images[0]} alt={product.title} />
+                <Link to={`/products/${product.id}`}>
+                  <h4>{product.title}</h4>
+                </Link>
+              </div>
+              <div className="product-details">
+                <Form.Group
+                  onChange={handleChange}
+                  className="product-amount"
+                  controlId="formGroupEmail"
+                >
+                  <Form.Label className="pr-1">Quantity:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    name={product.id}
+                    defaultValue={product.amount}
+                    as="select"
+                  >
+                    <option>{product.amount}</option>
+                    <option onClick={() => handleSubmit(product.id)}>1</option>
+                    <option onClick={() => handleSubmit(product.id)}>2</option>
+                    <option onClick={() => handleSubmit(product.id)}>3</option>
+                    <option onClick={() => handleSubmit(product.id)}>4</option>
+                    <option onClick={() => handleSubmit(product.id)}>5</option>
                   </Form.Control>
-                {/* {quantity[product.id] ?  <Button onClick={() => handleSubmit(product.id)} type="submit">
+                  {/* {quantity[product.id] ?  <Button onClick={() => handleSubmit(product.id)} type="submit">
                   Update
                 </Button> : <Button disabled type="submit">
                   Update
-                </Button> } */}               
-              </Form.Group>
-              <h5>Price: $ {product.price.toLocaleString()}</h5>
-            </div>
+                </Button> } */}
+                </Form.Group>
+                <h5>Price: $ {product.price.toLocaleString()}</h5>
+              </div>
+            </Col>
+          ))}
+        </Row>
+        <Row>
+          <Col
+            className="d-flex justify-content-end align-items-center"
+            xs={12}
+          >
+            <h4>Total: $ {totalValue.toLocaleString()}</h4>
+            <Button className="ml-2" variant="outline-primary">
+              Continue
+            </Button>
           </Col>
-        ))}
-      </Row>
-      <Row>
-        <Col className="d-flex justify-content-end align-items-center" xs={12}>
-          <h4>Total: $ {totalValue.toLocaleString()}</h4>
-          <Button className="ml-2" variant="outline-primary">
-            Continue
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
